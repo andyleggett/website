dd38606409f07f5486a361a68835dd3c7abd1e7e
Andrew Leggett <me@andyleggett.co.uk>
2016-05-26 13:14:27 +0100

    Changes to task monad article code

--- _scripts/charge.js
+++ _scripts/charge.js
@@ -14,6 +14,8 @@ import {
   curry,
   merge,
   identity,
+  sortBy,
+  head,
   __
 } from 'ramda';
 
@@ -27,15 +29,7 @@ var apiOptions = {
   distance: 10
 };
 
-const buildQueryOption = (items, item) => items + '&' + item[0] + '=' + item[1];
-const buildApiQueryString = compose(reduce(buildQueryOption, ''), toPairs);
-const mergeOptions = (location) => merge(__, compose(pick(['latitude', 'longitude']), prop('coords'))(location));
-
-const getLatLong = compose(pick(['Title', 'Distance', 'Latitude', 'Longitude']), prop(['AddressInfo']));
-const latOrLongMissing = or(propEq('Latitude', null), propEq('Longitude', null));
-const projectCoordinates = compose(map(createMarker), reject(latOrLongMissing), map(getLatLong));
-
-//Tasks
+//TASKS
 const getChargerData = (query) => new Task((rej, res) => {
   const req = new XMLHttpRequest();
   req.onreadystatechange = (e) => {
@@ -68,9 +62,22 @@ const getLocation = () => new Task((rej, res) => {
   }
 });
 
+const getDirections = (query) => new Task((rej, res) => {
+  const directionsService = new google.maps.DirectionsService();
+
+  directionsService.route(query, function(result, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      res(result);
+    } else {
+      rej(status);
+    }
+  });
+});
+
+//FUNCTIONS
 const fmap = map;
 
-const createMarker = (coord) => new googlemaps.Marker({
+const createMarker = (coord) => new google.maps.Marker({
   position: {
     lat: coord.Latitude,
     lng: coord.Longitude
@@ -79,7 +86,9 @@ const createMarker = (coord) => new googlemaps.Marker({
   icon: '/images/charger.png'
 });
 
-const createMap = curry((mapelem, location, markers) => {
+const setMarker = curry((map, marker) => marker.setMap(map));
+
+const createChargerMap = curry((mapelem, location, markers) => {
   var chargerMap = new google.maps.Map(mapelem, {
     center: new google.maps.LatLng(location.coords.latitude, location.coords.longitude),
     zoom: 13
@@ -94,13 +103,45 @@ const createMap = curry((mapelem, location, markers) => {
     title: 'You are here'
   });
 
+  map(setMarker(chargerMap))(markers);
+
   return chargerMap;
 });
 
-const mapMaker = (mapelem, location) => compose(fmap(createMap(mapelem, location)), fmap(projectCoordinates), getChargerData, buildApiQueryString, mergeOptions(location));
+const createClosestMap = curry((mapelem, location, directions) => {
+  var closestMap = new google.maps.Map(mapelem, {
+    center: new google.maps.LatLng(location.coords.latitude, location.coords.longitude),
+    zoom: 13
+  });
 
-const closestCharger = (googlemaps, mapobj, location) => {};
+  var directionsDisplay = new google.maps.DirectionsRenderer();
 
+  directionsDisplay.setMap(closestMap);
+  directionsDisplay.setDirections(directions);
+
+  return closestMap;
+});
+
+const buildQueryOption = (items, item) => items + '&' + item[0] + '=' + item[1];
+const buildApiQueryString = compose(reduce(buildQueryOption, ''), toPairs);
+const mergeOptions = (location) => merge(__, compose(pick(['latitude', 'longitude']), prop('coords'))(location));
+
+const getLatLong = compose(pick(['Title', 'Distance', 'Latitude', 'Longitude']), prop(['AddressInfo']));
+const latOrLongMissing = or(propEq('Latitude', null), propEq('Longitude', null));
+const projectCoordinates = compose(map(createMarker), reject(latOrLongMissing), map(getLatLong));
+
+const buildDirectionsQuery = curry((location, nearest) => ({
+  origin: new google.maps.LatLng(location.coords.latitude, location.coords.longitude),
+  destination: new google.maps.LatLng(nearest.Latitude, nearest.Longitude),
+  travelMode: google.maps.TravelMode.DRIVING
+}));
+
+const getNearestCharger = compose(head, sortBy(prop('Distance')), reject(latOrLongMissing), map(getLatLong));
+
+const mapMaker = (mapelem, location) => compose(fmap(createChargerMap(mapelem, location)), fmap(projectCoordinates), getChargerData, buildApiQueryString, mergeOptions(location));
+const closestCharger = (mapelem, location) => compose(fmap(createClosestMap(mapelem, location)), chain(getDirections), fmap(buildDirectionsQuery(location)), fmap(getNearestCharger), getChargerData, buildApiQueryString, mergeOptions(location));
+
+//PROGRAM START
 const distanceSlider = document.getElementById('distance-slider');
 const resultCountSlider = document.getElementById('result-count-slider');
 const distanceDisplay = document.getElementById('distance-display');
@@ -131,24 +172,9 @@ resultCountSlider.addEventListener('change', resultsChanged);
 
 getLocation().fork(error, (location) => {
 
-  /*var closestMap = new google.maps.Map(document.getElementById('closest-map'), {
-    center: new google.maps.LatLng(location.coords.latitude, location.coords.longitude),
-    zoom: 13
-  });
-
-  var closestCentreMarker = new google.maps.Marker({
-    position: {
-      lat: location.coords.latitude,
-      lng: location.coords.longitude
-    },
-    map: closestMap,
-    title: 'You are here'
-  });*/
-
   currentMapMaker = mapMaker(document.getElementById('charger-map'), location);
 
-  currentMapMaker(apiOptions).fork(error, log);
+  currentMapMaker(apiOptions).fork(error, identity);
 
-  //closestCharger(google.maps, closestMap, location).fork(error, log);
+  closestCharger(document.getElementById('closest-map'), location)(apiOptions).fork(error, log);
 });
-
